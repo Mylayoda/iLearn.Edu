2d6bef2cb8c26791e2ee46a2f0084277ea4a5797
Derrick Yirenkyi <21703340@bucks.ac.uk>
2020-04-13 07:21:34 +0100

    Add project files.

---
+++ iLearn.Edu/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iLearn.Edu.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
